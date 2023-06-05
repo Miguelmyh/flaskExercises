3ef58c526229ea01d2063872206f816384fdd66f
Miguel <miguelmyh14@gmail.com>
2023-06-05 19:06:43 -0400

    finish the exercises

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,60 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+
+@app.route("/add")
+def do_add():
+    """Add a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+
+    return str(result)
+
+@app.route("/sub")
+def do_sub():
+    """Subtract and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+
+    return str(result)
+
+@app.route("/mult")
+def do_mult():
+    """Multiply a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+
+    return str(result)
+
+@app.route("/div")
+def divide():
+    """Divide a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+
+    return str(result)
+
+operations = {
+    'add': add,
+    'sub': sub,
+    'div': div,
+    'mult': mult,
+}
+
+@app.route('/math/<function>')
+def process(function):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operations[function](a, b)
+    return str(result)
